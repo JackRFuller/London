35c1888e46fb10c03a4e12867e739cf4ef49a1d1
JackRFuller <jackrfuller@outlook.com>
2018-09-11 20:27:05 +0200

    Imported TMP & Started Network Manager

---
+++ Assets/Scripts/Managers/GlobalManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GlobalManager : MonoBehaviour 
+{
+	private static GlobalManager instance;
+	public static GlobalManager Instance 
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	private void Awake()
+	{
+        instance = this;
+
+        if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+}
---
+++ Assets/Scripts/Managers/NetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class NetworkManager : Photon.MonoBehaviour
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    public virtual void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+    }
+    
+
+
+}
